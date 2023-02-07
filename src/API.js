a948a885edb1f44f09664c84ec9eb991373b2272
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-02-07 09:49:34 +0530

    added header for API.js file to check post method

--- src/API.js
+++ src/API.js
@@ -2,8 +2,18 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const AxiosPost = async (payload) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
   try {
-    const res = await axios.post(process.env.REACT_APP_BASE_URL, payload);
+    const res = await axios.post(
+      process.env.REACT_APP_BASE_URL,
+      payload,
+      config
+    );
     console.log("🚀 ~ file: API.js:6 ~ AxiosPost ~ res", res);
     const data = await res.data;
     console.log("🚀 ~ file: API.js:9 ~ AxiosPost ~ data", data);
